e0151fe1f58e249457eb07451419e18921e56f03
Konrad <17689319+konradsmolko@users.noreply.github.com>
2019-06-09 17:59:09 +0200

    preparation for multiprocessing

--- HPAPROX/main.py
+++ HPAPROX/main.py
@@ -13,7 +13,7 @@ def interpolate_point_lagrange(xi, data: [Data]):
                 term *= (xi - data[j].x)/float(data[i].x - data[j].x)
         result += term
 
-    print("Interpolated point", xi)
+    print("Interpolated point", xi, "result", result)
     return result
 
 
@@ -22,8 +22,34 @@ def interpolate_lagrange(data: [Data], interpolation_range=None) -> [Data]:
         interpolation_range = len(data)
 
     lin_x = linspace(data[0].x, data[-1].x, interpolation_range)
-
+    print("Max:", lin_x[-1])
     return [Data(lin_x[i], interpolate_point_lagrange(lin_x[i], data)) for i in range(interpolation_range)]
+
+
+# def approx_polynomial(data: [Data], deg=3):
+#     n = len(data)
+#     arr = [[0.0 for _ in range(deg)] for _ in range(deg)]
+#     r = [0.0 for _ in range(deg)]
+#     # d = [0.0 for _ in range(deg)]
+#
+#     # Konstrukcja M
+#     for i in range(deg):
+#         for j in range(deg):
+#             arr[i][j] = sum([p.x ** (i+j) for p in data])
+#
+#     # Konstrukcja d
+#     d = [sum([(p.x ** i) * p.y for p in data]) for i in range(deg)]
+#     # for i in range(deg):
+#     #     d[i] = sum([(p.x ** i) * p.y for p in data])
+#
+#     # Rozwiązanie r
+#     pass
+
+
+def interpolate_splines(data: [Data], interpolation_range=None) -> [Data]:
+    if interpolation_range is None:
+        interpolation_range = len(data)
+    pass
 
 
 def process(dataset: [Data]):
@@ -35,12 +61,12 @@ def process(dataset: [Data]):
     # )
     # plt.show()
 
-    interpolated_data = interpolate_lagrange(dataset, len(dataset)*2)
+    interpolated_data = interpolate_lagrange(dataset, len(dataset)*10)
 
     plt.plot(
         [point.x for point in dataset],
         [point.y for point in dataset],
-        'b.',
+        'b-',
         label='raw dataset'
     )
     plt.plot(
@@ -50,6 +76,7 @@ def process(dataset: [Data]):
         label='interpolated'
     )
     plt.legend()
+    plt.ylim(min([p.y for p in dataset]) - 10, max([p.y for p in dataset]) + 10)
     # plt.yscale("log")
     plt.show()
 
@@ -70,7 +97,8 @@ def single_file(filename='./data/100.txt'):
 
 def main():
     # all_data()
-    single_file('./data/Redlujjj.txt')  # largest file
+    single_file('./data/SpacerniakGdansk.txt')
+    # single_file('./data/test.txt')
 
 
 if __name__ == '__main__':
